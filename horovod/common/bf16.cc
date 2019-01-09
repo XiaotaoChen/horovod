ca811609d09866587b9a7d77043dac295ceb6fcd
XiaotaoChen <xiaotao.chen@intel.com>
2019-01-09 20:35:13 +0800

    support intrinsic sum

--- horovod/common/bf16.cc
+++ horovod/common/bf16.cc
@@ -19,6 +19,11 @@ bool check_equal(const unsigned int a, const uint16_t b){
   return short_a == b;
 }
 
+bool is_aligned(const void* ptr, int alignment) {
+  auto iptr = reinterpret_cast<uintptr_t>(ptr);
+  return !(iptr % alignment);
+}
+
 float cal_var_range(const unsigned int a, const uint16_t b){
   const float* fp_a = reinterpret_cast<const float*>(&a);
   const unsigned int int_b = b<<16;
@@ -55,26 +60,13 @@ void BF16ToFloat(const uint16_t* src, float* dst, int len, int type_flag){
  {
    case 0:
      {
-       int i = 0;
-       for(; i < (len / 16) * 16; i += 16){
-         convert_b16_to_f32(*(__m256i*)(src+i), (__m512i*)(dst+i));
+       int i;
+       for(i = 0; i < (len / 16) * 16; i += 16){
+         convert_b16_to_f32((const void*)(src+i), (void*)(dst+i));
        }
        // process the remaining data
        unsigned int* dst_unsigned = reinterpret_cast<unsigned int*>(dst);
-       for(; i < len; i++){
-         *(dst_unsigned+i) = *(src+i)<<16;
-       }
-     }
-     break;
-   case 1:
-     {
-       int i = 0;
-       for(; i < (len / 16) * 16; i += 16){
-         convert_b16_to_f32(*(__m256i*)(src+i), (__m256i*)(dst+i), (__m256i*)(dst+i+8));
-       }
-       // process the remaining data
-       unsigned int* dst_unsigned = reinterpret_cast<unsigned int*>(dst);
-       for(; i < len; i++){
+       for(i = (len / 16) * 16; i < len; i++){
          *(dst_unsigned+i) = *(src+i)<<16;
        }
      }
@@ -82,7 +74,8 @@ void BF16ToFloat(const uint16_t* src, float* dst, int len, int type_flag){
    default:
      {
        unsigned int* dst_unsigned = reinterpret_cast<unsigned int*>(dst);
-       for(int i=0; i < len; i++){
+       int i;
+       for(i=0; i < len; i++){
          *(dst_unsigned+i) = *(src+i)<<16;
        }
      }
@@ -91,30 +84,25 @@ void BF16ToFloat(const uint16_t* src, float* dst, int len, int type_flag){
 }
 
 void FloatToBF16(const float* src, uint16_t* dst, int len, int type_flag){
+ bool aligned_flag = is_aligned(reinterpret_cast<const void*>(src), 64)
+                     && is_aligned(reinterpret_cast<const void*>(dst), 64);
  switch (type_flag)
  {
    case 0:
      {
-       int i = 0;
-       for(; i < (len / 16) * 16; i += 16){
-         convert_f32_to_b16(*(__m512i*)(src+i), (__m256i*)(dst+i));
+       int i;
+       if (aligned_flag) {
+         for(i = 0; i < (len / 16) * 16; i += 16){
+           convert_f32_to_b16((__m512i*)(src+i), (__m256i*)(dst+i));
+         }
+       } else {
+         for(i = 0; i < (len / 16) * 16; i += 16){
+           convert_f32_to_b16((const void*)(src+i), (void*)(dst+i));
+         }
        }
        // process the remaining data
        const unsigned int* src_unsigned = reinterpret_cast<const unsigned int*>(src);
-       for(; i < len; i++){
-         *(dst+i) = *(src_unsigned+i)>>16;
-       }
-     }
-     break;
-   case 1:
-     {
-       int i = 0;
-       for(; i < (len / 16) * 16; i += 16){
-         convert_f32_to_b16(*(__m256i*)(src+i), *(__m256i*)(src+i+8), (__m256i*)(dst+i));
-       }
-       // process the remaining data
-       const unsigned int* src_unsigned = reinterpret_cast<const unsigned int*>(src);
-       for(; i < len; i++){
+       for(i = (len / 16) * 16; i < len; i++){
          *(dst+i) = *(src_unsigned+i)>>16;
        }
      }
@@ -122,7 +110,8 @@ void FloatToBF16(const float* src, uint16_t* dst, int len, int type_flag){
    default:
      {
        const unsigned int* src_unsigned = reinterpret_cast<const unsigned int*>(src);
-       for(int i=0; i < len; i++){
+       int i;
+       for(i=0; i < len; i++){
          *(dst+i) = *(src_unsigned+i)>>16;
        }
      }
@@ -131,42 +120,41 @@ void FloatToBF16(const float* src, uint16_t* dst, int len, int type_flag){
 }
 
 void bf16_sum(void* invec, void* inoutvec, int* len, MPI_Datatype* datatype){
-  int type_flag = 0;
-  int i=0;
+  bool aligned_flag = is_aligned(reinterpret_cast<const void*>(invec), 64)
+                      && is_aligned(reinterpret_cast<const void*>(inoutvec), 64);
+  int i;
   uint16_t* invec_16 = reinterpret_cast<uint16_t*>(invec);
   uint16_t* inoutvec_16 = reinterpret_cast<uint16_t*>(inoutvec);
+  int type_flag = 0;
   if(type_flag == 0){
-    for(; i < (*len / 16) * 16; i += 16)
-    {
-      // convert in & inout to m512
-      __m512i in_m512 = _mm512_bslli_epi128(_mm512_cvtepu16_epi32(*(__m256i*)(invec_16+i)), 2);
-      __m512i out_m512 = _mm512_bslli_epi128(_mm512_cvtepu16_epi32(*(__m256i*)(inoutvec_16+i)), 2);
-      // add them together to new_inout_m256
-      __m512 newout_m512 = _mm512_add_ps((__m512)in_m512, (__m512)out_m512);
-      // convert back and store in inout
-      *(__m256i*)(inoutvec_16 + i) = _mm512_cvtepi32_epi16(_mm512_bsrli_epi128((__m512i)newout_m512, 2));
-    }
-  } else if(type_flag == 1){
-    // alignas 64 for __m256i requested
-    alignas(64) int zero[8] = {0,0,0,0,0,0,0,0};
-    __m256i zeros = *(__m256i*)zero;
-    for(; i< (*len / 16) * 16; i += 16){
-      // convert in & out to m256
-      __m256i invec0 = _mm256_unpacklo_epi16(zeros, *(__m256i*)(invec_16 + i));
-      __m256i invec1 = _mm256_unpackhi_epi16(zeros, *(__m256i*)(invec_16 + i));
-      __m256i outvec0 = _mm256_unpacklo_epi16(zeros, *(__m256i*)(inoutvec_16 + i));
-      __m256i outvec1 = _mm256_unpackhi_epi16(zeros, *(__m256i*)(inoutvec_16 + i));
-      // add them together to new_inout_m256
-      __m256 new_inout0_m256 = _mm256_add_ps((__m256)invec0, (__m256)outvec0);
-      __m256 new_inout1_m256 = _mm256_add_ps((__m256)invec1, (__m256)outvec1);
-      // convert back and store in inout
-      __m256i inout0_m256i = _mm256_srli_epi32((__m256i)new_inout0_m256, 16);
-      __m256i inout1_m256i = _mm256_srli_epi32((__m256i)new_inout1_m256, 16);
-      *(__m256i*)(inoutvec_16 + i) = _mm256_packus_epi32(inout0_m256i, inout1_m256i);
+    if (aligned_flag) {
+      for(i=0; i < (*len / 16) * 16; i += 16)
+      {
+        // convert in & inout to m512
+        __m512i in_m512, out_m512;
+        convert_b16_to_f32((const void*)(invec_16+i), (void*)(&in_m512));
+        convert_b16_to_f32((const void*)(inoutvec_16+i), (void*)(&out_m512));
+        // add them together to new_inout_m256
+        __m512 newout_m512 = _mm512_add_ps((__m512)in_m512, (__m512)out_m512);
+        // convert back and store in inout
+        convert_f32_to_b16((__m512i*)(&newout_m512), (__m256i*)(inoutvec_16+i));
+      }
+    } else {
+      for(i=0; i < (*len / 16) * 16; i += 16)
+      {
+        // convert in & inout to m512
+        __m512i in_m512, out_m512;
+        convert_b16_to_f32((const void*)(invec_16+i), (void*)(&in_m512));
+        convert_b16_to_f32((const void*)(inoutvec_16+i), (void*)(&out_m512));
+        // add them together to new_inout_m256
+        __m512 newout_m512 = _mm512_add_ps((__m512)in_m512, (__m512)out_m512);
+        // convert back and store in inout
+        convert_f32_to_b16((const void *)(&newout_m512), (void*)(inoutvec_16+i));
+      }
     }
   }
   // process the remaining data
-  for(; i < *len; i++){
+  for(i=(*len / 16) * 16; i < *len; i++){
     unsigned int tmp_in = (*(invec_16 + i)) << 16;
     unsigned int tmp_out = (*(inoutvec_16 + i)) << 16;
     float in_float = *reinterpret_cast<float*>(&tmp_in);
--- horovod/common/bf16.h
+++ horovod/common/bf16.h
@@ -21,43 +21,37 @@ namespace common {
 inline uint16_t* bf16_alloc(size_t size){
   // TODO set alignment to 256/512, it's performance may be better.
   //size must be an integral multiple of 64.
-  if (size % 256 == 0) {
-    return reinterpret_cast<uint16_t*>(aligned_alloc(256, size));
+  if (size % 64 == 0) {
+    return reinterpret_cast<uint16_t*>(aligned_alloc(64, size));
   }
   else {
-    size_t new_size = (size / 256 + 1 ) * 256;
-    return reinterpret_cast<uint16_t*>(aligned_alloc(256, new_size));
+    size_t new_size = (size / 64 + 1 ) * 64;
+    return reinterpret_cast<uint16_t*>(aligned_alloc(64, new_size));
   }
 }
 
-inline void convert_f32_to_b16(__m512i src, __m256i* dst)
+inline void convert_f32_to_b16(const void* src, void* dst)
 {
-  __m512i y = _mm512_bsrli_epi128(src, 2);
+  __m512i y = _mm512_bsrli_epi128(_mm512_loadu_si512(src), 2);
+  _mm256_storeu_si256((__m256i*)(dst), _mm512_cvtepi32_epi16(y));
+}
+
+// when the pointer is aligned, it needn't to call movdqu
+inline void convert_f32_to_b16(__m512i* src, __m256i* dst)
+{
+  __m512i y = _mm512_bsrli_epi128(*src, 2);
   *dst = _mm512_cvtepi32_epi16(y);
 }
 
-inline void convert_b16_to_f32(__m256i src, __m512i* dst)
+inline void convert_b16_to_f32(const void* src, void* dst)
 {
-  __m512i y = _mm512_cvtepu16_epi32(src);
-  *dst = _mm512_bslli_epi128(y, 2);
-}
-
-inline void convert_f32_to_b16(__m256i src0, __m256i src1, __m256i *dst)
-{
-    src0 = _mm256_srli_epi32(src0, 16);
-    src1 = _mm256_srli_epi32(src1, 16);
-    *dst = _mm256_packus_epi32(src0, src1);
-}
-
-inline void convert_b16_to_f32(__m256i src, __m256i *dst0, __m256i *dst1)
-{
-    int zero[8] = {0,0,0,0,0,0,0,0};
-    __m256i zeros = *(__m256i*)zero;
-    *dst0 = _mm256_unpacklo_epi16(zeros, src);
-    *dst1 = _mm256_unpackhi_epi16(zeros, src);
+  __m512i y = _mm512_cvtepu16_epi32(_mm256_loadu_si256((__m256i const*)src));
+  _mm512_storeu_si512(dst, _mm512_bslli_epi128(y, 2));
 }
 
 bool check_equal(const unsigned int a, const uint16_t b);
+
+bool is_aligned(const void* ptr, int alignment);
 
 float cal_var_range(const unsigned int a, const uint16_t b);
 
